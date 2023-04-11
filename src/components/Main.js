ea0534c623b95bb3513411ca66e0f15dbafb17a6
Artem Mazin <minima1s@yandex.ru>
2023-04-11 23:24:04 +0300

    feat: добавить императивные обработчики для попапов

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,25 @@
+import avatar from '../images/kusto.jpg';
+
 function Main(props) {
+  const handleEditProfileClick = () => {
+    document.querySelector('#popup-profile').classList.add('popup_opened');
+  };
+  const handleEditAvatarClick = () => {
+    document.querySelector('#popup-avatar').classList.add('popup_opened');
+  };
+  const handleAddPlaceClick = () => {
+    document.querySelector('#popup-cards').classList.add('popup_opened');
+  };
+
   return (
     <main className='main wrapper'>
       <section className='profile'>
         <div className='profile__content'>
-          <div className='profile__avatar-container'>
+          <div
+            className='profile__avatar-container'
+            onClick={handleEditAvatarClick}>
             <img
-              src="<%=require('./images/kusto.jpg')%>"
+              src={avatar}
               alt='Аватар профиля'
               className='profile__avatar'
             />
@@ -16,14 +30,16 @@ function Main(props) {
               <h1 className='profile__name'></h1>
               <button
                 className='profile__edit-button'
-                type='button'></button>
+                type='button'
+                onClick={handleEditProfileClick}></button>
             </div>
             <p className='profile__job'></p>
           </div>
         </div>
         <button
           className='profile__add-button'
-          type='button'></button>
+          type='button'
+          onClick={handleAddPlaceClick}></button>
       </section>
       <section className='cards'>
         <ul className='cards__items'></ul>
