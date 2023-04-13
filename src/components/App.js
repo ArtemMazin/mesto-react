6b3e685085466b7272ca8a46597381ab505cf711
Artem Mazin <minima1s@yandex.ru>
2023-04-13 23:04:49 +0300

    feat: создать общий компонент попапов PopupWithForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import '../index.css';
-import Footer from './Footer';
-import Header from './Header';
-import Main from './Main';
+import Footer from './Footer.js';
+import Header from './Header.js';
+import Main from './Main.js';
+import PopupWithForm from './PopupWithForm.js';
 
 function App() {
   return (
@@ -9,7 +10,94 @@ function App() {
       <Header />
       <Main />
       <Footer />
-
+      <PopupWithForm
+        title={'Редактировать профиль'}
+        name={'profile'}
+        buttonName={'Сохранить'}>
+        <label>
+          <input
+            className='popup__input popup__input_type_name'
+            type='text'
+            name='name'
+            // value=''
+            placeholder='Введите имя'
+            required
+            minlength='2'
+            maxlength='40'
+          />
+          <span
+            className='popup__input-error'
+            id='name-error'></span>
+          <input
+            className='popup__input popup__input_type_job'
+            type='text'
+            name='job'
+            // value=''
+            placeholder='Введите профессию'
+            required
+            minlength='2'
+            maxlength='200'
+          />
+          <span
+            className='popup__input-error'
+            id='job-error'></span>
+        </label>
+      </PopupWithForm>
+      <PopupWithForm
+        title={'Новое место'}
+        name={'cards'}
+        buttonName={'Создать'}>
+        <label>
+          <input
+            className='popup__input popup__input_type_name'
+            id='name-cards__input'
+            type='text'
+            name='cards_input_name'
+            // value=''
+            placeholder='Введите название'
+            required
+            minlength='2'
+            maxlength='30'
+          />
+          <span
+            className='popup__input-error'
+            id='cards_input_name-error'></span>
+          <input
+            className='popup__input popup__input_type_job'
+            id='link-cards__input'
+            type='url'
+            name='cards_input_link'
+            // value=''
+            placeholder='Введите ссылку'
+            required
+          />
+          <span
+            className='popup__input-error'
+            id='cards_input_link-error'></span>
+        </label>
+      </PopupWithForm>
+      <PopupWithForm
+        title={'Обновить аватар'}
+        name={'avatar'}
+        buttonName={'Сохранить'}>
+        <label>
+          <input
+            className='popup__input popup__input_type_name'
+            name='avatar'
+            type='url'
+            // value=''
+            placeholder='Введите ссылку на изображение'
+            required
+          />
+          <span
+            className='popup__input-error'
+            id='avatar-error'></span>
+        </label>
+      </PopupWithForm>
+      <PopupWithForm
+        title={'Вы уверены?'}
+        name={'delete'}
+        buttonName={'Да'}></PopupWithForm>
       <template id='card-template'>
         <li className='cards__item'>
           <button
@@ -31,154 +119,7 @@ function App() {
           </div>
         </li>
       </template>
-      <div
-        className='popup popup_background_light'
-        id='popup-profile'>
-        <div className='popup__container'>
-          <h2 className='popup__title'>Редактировать профиль</h2>
-          <form
-            className='popup__form'
-            name='form_profile'
-            id='form-profile'
-            novalidate>
-            <input
-              className='popup__input popup__input_type_name'
-              type='text'
-              name='name'
-              value=''
-              placeholder='Введите имя'
-              required
-              minlength='2'
-              maxlength='40'
-            />
-            <span
-              className='popup__input-error'
-              id='name-error'></span>
-            <input
-              className='popup__input popup__input_type_job'
-              type='text'
-              name='job'
-              value=''
-              placeholder='Введите профессию'
-              required
-              minlength='2'
-              maxlength='200'
-            />
-            <span
-              className='popup__input-error'
-              id='job-error'></span>
-            <button
-              className='popup__button-submit popup__button-submit_disabled'
-              type='submit'>
-              Сохранить
-            </button>
-          </form>
-          <button
-            className='popup__close-btn'
-            type='button'></button>
-        </div>
-      </div>
-      <div
-        className='popup popup_background_light'
-        id='popup-cards'>
-        <div className='popup__container'>
-          <h2 className='popup__title'>Новое место</h2>
-          <form
-            className='popup__form'
-            name='form_cards'
-            id='form-cards'
-            novalidate>
-            <input
-              className='popup__input popup__input_type_name'
-              id='name-cards__input'
-              type='text'
-              name='cards_input_name'
-              value=''
-              placeholder='Введите название'
-              required
-              minlength='2'
-              maxlength='30'
-            />
-            <span
-              className='popup__input-error'
-              id='cards_input_name-error'></span>
-            <input
-              className='popup__input popup__input_type_job'
-              id='link-cards__input'
-              type='url'
-              name='cards_input_link'
-              value=''
-              placeholder='Введите ссылку'
-              required
-            />
-            <span
-              className='popup__input-error'
-              id='cards_input_link-error'></span>
-            <button
-              className='popup__button-submit popup__button-submit_disabled'
-              type='submit'>
-              Создать
-            </button>
-          </form>
-          <button
-            className='popup__close-btn'
-            type='button'
-            id='popup-cards__close-btn'></button>
-        </div>
-      </div>
-      <div
-        className='popup popup_background_light'
-        id='popup-avatar'>
-        <div className='popup__container'>
-          <h2 className='popup__title'>Обновить аватар</h2>
-          <form
-            className='popup__form'
-            name='form_avatar'
-            id='form-avatar'
-            novalidate>
-            <input
-              className='popup__input popup__input_type_name'
-              name='avatar'
-              type='url'
-              value=''
-              placeholder='Введите ссылку на изображение'
-              required
-            />
-            <span
-              className='popup__input-error'
-              id='avatar-error'></span>
-            <button
-              className='popup__button-submit popup__button-submit_disabled'
-              type='submit'>
-              Сохранить
-            </button>
-          </form>
-          <button
-            className='popup__close-btn'
-            type='button'></button>
-        </div>
-      </div>
-      <div
-        className='popup popup_background_light'
-        id='popup-delete'>
-        <div className='popup__container'>
-          <h2 className='popup__title'>Вы уверены?</h2>
-          <form
-            className='popup__form'
-            name='form_delete'
-            id='form-delete'
-            novalidate>
-            <button
-              className='popup__button-submit'
-              type='submit'>
-              Да
-            </button>
-          </form>
-          <button
-            className='popup__close-btn'
-            type='button'></button>
-        </div>
-      </div>
+
       <div
         className='popup popup_background_dark'
         id='popup-image'>
